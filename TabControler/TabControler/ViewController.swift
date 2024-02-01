f8672c905be04ece03216d47826cdec9bdf3e465
Jorge Reyes <142442550+jorgesoftdev@users.noreply.github.com>
2024-02-01 11:02:40 -0600

    Add files via upload

---
+++ TabControler/TabControler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabControler
+//
+//  Created by Jorge on 01/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TabControler/TabControlerUITests/TabControlerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TabControlerUITestsLaunchTests.swift
+//  TabControlerUITests
+//
+//  Created by Jorge on 01/02/24.
+//
+
+import XCTest
+
+final class TabControlerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
